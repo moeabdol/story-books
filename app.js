86490ef62b7b958e110cf8b30cec6a64b05f21b8
moeabdol <mohd.a.saed@gmail.com>
2017-10-25 21:17:10 +0300

    Implement basic app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('index page');
+});
+
+app.listen(PORT, err => {
+  if (err) return console.log(err);
+  console.log('Server listening on port', PORT);
+});
