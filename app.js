fc13937bfa20eb3930cac7944fd7f8e567ffd90e
moeabdol <mohd.a.saed@gmail.com>
2017-10-25 23:24:21 +0300

    Install passport and configure google oauth

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 require('dotenv').config();
 
-const express = require('express');
+const express    = require('express');
+const passport   = require('passport');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 const PORT = 3000;
 
+// Configure passport
+require('./config/passport')(passport);
+app.use(passport.initialize());
+
 app.get('/', (req, res) => {
   res.send('index page');
 });
+
+app.use('/auth', authRoutes);
 
 app.listen(PORT, err => {
   if (err) return console.log(err);
