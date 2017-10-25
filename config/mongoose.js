2f418f19d3ef578c423b36ab5e535cd3c7cccb6a
moeabdol <mohd.a.saed@gmail.com>
2017-10-25 22:56:36 +0300

    Configure mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,1 +1,9 @@
 const mongoose = require('mongoose');
+const config = require('./');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.db, { useMongoClient: true })
+  .then(() => console.log('Connected to', config.db))
+  .catch(err => console.log(err));
+
+module.exports = mongoose;
