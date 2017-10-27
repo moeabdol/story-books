fb660ea120f0f37645f9d173ae4e1c8f47c5aca3
moeabdol <mohd.a.saed@gmail.com>
2017-10-27 15:33:36 +0300

    Implement basic story model

---
+++ models/story.js
@@ -0,0 +1,51 @@
+const mongoose = require('../config/mongoose');
+const Schema   = mongoose.Schema;
+
+const StorySchema = Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true,
+    default: 'public'
+  },
+  allowComments: {
+    type: Boolean,
+    required: true,
+    default: true
+  },
+  comments: [{
+    body: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: Date,
+      required: true,
+      default: Date.now
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'users',
+      required: true
+    }
+  }],
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+    required: true
+  },
+  date: {
+    type: Date,
+    requierd: true,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Story', StorySchema);
